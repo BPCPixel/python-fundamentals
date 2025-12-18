1daf041e452f6eb194430d97ca00f30b1464a474
Lalo Tellez <bpcpixel@gmail.com>
2025-12-17 23:02:23 -0600

    Add data type conversion example with numeric addition and string concatenation

---
+++ Strings/data_types_converter.py
@@ -0,0 +1,16 @@
+# Data Type Conversion Program
+
+# We define a string type variable
+text_number = "50"
+
+# Convert string to integer and add 10
+number = int(text_number) + 10
+
+# Printing numeric result
+print("Numeric Result:", number)
+
+# Concatenate strings
+concatenation = text_number + str(10)
+
+# Printing concatenation result
+print("Text Result:", concatenation)
