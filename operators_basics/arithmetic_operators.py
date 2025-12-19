ef1e375c45474f1dc7ac916d0e857810a3bbb593
Lalo Tellez <bpcpixel@gmail.com>
2025-12-19 01:27:58 -0600

    Add arithmetic operators example with static values

---
+++ operators_basics/arithmetic_operators.py
@@ -0,0 +1,38 @@
+# Arithmetic operators
+
+a = 10
+b = 3
+
+print(f'{'=' * 20} Arithmetic operators {'=' * 20}')
+
+print(f' A = {a} , B = {b}\n')
+
+# Addition
+addition = a + b
+print(f'Addition of {a} + {b} = {addition}')
+
+# Subtraction
+subtraction = a - b
+print(f'Subtraction of {a} - {b} = {subtraction}')
+
+# Multiplication
+multiplication = a * b
+print(f'Multiplication of {a} * {b} = {multiplication}')
+
+# Division
+division = a / b
+print(f'Division of {a} / {b} = {division}') # returns a float number
+
+# Floor Division result
+floor_division = a // b
+print(f'Floor division result of {a} // {b} = {floor_division}') # returns an integer number
+
+# Modulus: ramaremainder value
+modulus = a % b
+print(f'Modulus of {a} % {b} = {modulus}')
+
+# Exponentiation
+exponentiation = a ** b
+print(f'Exponentiation of {a} raised to {b} = {exponentiation}\n')
+
+print(f'{"=" * 60}')
