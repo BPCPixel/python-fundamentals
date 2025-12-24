99d4761bb5b8850ac63f2c72516514e5cbf54050
Lalo Tellez <bpcpixel@gmail.com>
2025-12-23 22:03:35 -0600

    Adding an update to the program where i show to the user the allowed distance and switching the integer 3 to a variable called allowed_distance where i declare 3 as a valid distance

--- operators_basics/operators_practices/loan_book_system.py
+++ operators_basics/operators_practices/loan_book_system.py
@@ -3,14 +3,19 @@
 # 1. The user has a student credential
 # 2. The user lives no more than 3 km away
 
-print(f'{"=" * 20} Book Loan System {"=" * 20}\n')
+print(f'''{"=" * 20} Book Loan System {"=" * 20}
+
+Allowed distance: 3KM''')
+
+# Valid distance
+allowed_distance = 3
 
 # User input
 credential = input("Do you have a student credential? (Y/N): ").strip().upper()
 distance_km = int(input("How many kilometers away do you live?: "))
 
 # Logical validation
-loan_book = (credential == "Y" or distance_km <= 3)
+loan_book = (credential == "Y" or distance_km <= allowed_distance)
 
 # Result
 print(f'''
