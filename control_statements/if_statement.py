0b8967abb3a6a853c05b13bb7d6f8dcd899d5e4e
Lalo Tellez <bpcpixel@gmail.com>
2025-12-25 23:25:51 -0600

    Basic if-else age validation

---
+++ control_statements/if_statement.py
@@ -0,0 +1,12 @@
+# This program explains how the if statement works
+
+print(f'{"=" * 20} If Statement {"=" * 20}\n')
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+print(f'\n{"=" * 50}')
