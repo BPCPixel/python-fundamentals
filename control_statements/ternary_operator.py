853f06414a0000d57f7577a3c75c2bdc51f0d3ec
Lalo Tellez <bpcpixel@gmail.com>
2025-12-26 19:54:05 -0600

    Practice ternary operator with age validation

---
+++ control_statements/ternary_operator.py
@@ -0,0 +1,13 @@
+# This program explains how the ternary operator works
+print(f'{"=" * 20} Ternary Operator {"=" * 20}\n')
+
+# User input
+age = int(input('Age: '))
+
+# Ternary operator
+is_adult = 'Yes' if age >= 18 else 'No'
+
+# Output
+print(f'Is an adult? {is_adult}')
+
+print(f'\n{"=" * 50}')
