e4113feec23bc1b3757a783957cf0415074132b9
Lalo Tellez <bpcpixel@gmail.com>
2025-12-30 00:08:12 -0600

    Add basic while loop example printing numbers from 1 to 5

---
+++ loops/while_loop.py
@@ -0,0 +1,20 @@
+"""
+While Loop Example
+
+This program demonstrates how a while loop works
+by printing numbers from 1 to 5.
+
+Author: Lalo Tellez
+"""
+
+print(f'{"=" * 20} While Loop Example {"=" * 20}\n')
+print('Printing values from 1 to 5:')
+
+# Initialize counter
+counter = 1
+
+while counter <= 5:
+    print(counter, end=' ')
+    counter += 1  # Increment counter
+
+print(f'\n{"=" * 50}')
