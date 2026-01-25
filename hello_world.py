0549f5903817d747541004a98875e1ef7f7878ab
Lalo Tellez <bpcpixel@gmail.com>
2026-01-24 21:08:50 -0600

    Starting a practice to use the General Formula

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,1 @@
 print('Hello World')
-
-print('TESTING GIT COMMANDS')
---
+++ my_projects/general_formula.py
@@ -0,0 +1,16 @@
+"""
+Practice: create a program to use the general formula
+
+Author: Lalo Téllez
+"""
+
+print(f'{"=" * 20} General Formula {"=" * 20}')
+
+a = float(input("A: "))
+b = float(input("B: "))
+c = float(input("C: "))
+
+
+
+
+print(f'{"=" * 50}')
